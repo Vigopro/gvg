f2b637b36d4fd48f34c8bb8d5539d3eafc7504a2
Vitaliy <23neyron@gmail.com>
2016-03-13 16:09:26 +0300

    добавил форму проверки add form validation

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,12 @@ class SignUpForm(forms.ModelForm):
 		model = SignUp
 		fields = ['full_name', 'email']
 #		exclude = ['full_name']
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split('@')
+		domain, extension = provider.split('.')
+		if not domain == 'USC':
+			raise forms.ValidationError("Пожалуйста, убедитесь, что вы используете ваш USC электронной почты.")
+		if not extension == 'edu':
+			raise forms.ValidationError("Пожалуйста, используйте действительный адрес электронной почты .edu")
+		return email
